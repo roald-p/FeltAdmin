c91a33449c697a0a378e1af0e9c11ea713c7833e
lnystad <lan@dips.no>
2017-06-21 17:16:50 +0200

    Lagt på litt test :)

--- Test/FeltAdminTest/Orion/OrionGeneratePausedTests.cs
+++ Test/FeltAdminTest/Orion/OrionGeneratePausedTests.cs
@@ -12,40 +12,51 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace FeltAdminTest.Orion
 {
     [TestClass]
-    public class OrionGenerateTests
+    public class OrionGeneratePausedTests
     {
         [TestMethod]
         public void FirstTarget_FirstTeam()
         {
-            var settings = TestDataHelper.GetSettings("FeltAdminTest.TestData.FeltAdmin.xml");
+            var settings = TestDataHelper.GetSettings("FeltAdminTest.TestData.FeltAdmin2OrionPauses.xml");
 
             var leonPerson = new LeonPerson
                              {
                                  Class = "4",
                                  ClubName = "Bodø Østre",
                                  Name = "Roald Parelius",
-                                 Range = 2,
+                                 Range = 1,
                                  ShooterId = 123,
                                  SumIn = 0,
                                  Target = 1,
                                  Team = 1
                              };
 
+            foreach (var orionView in settings.OrionSetting.OrionViewModels)
+            {
+                foreach (var range in orionView.RangeViews)
+                {
+                    range.Parent = orionView;
+                }
+            }
+
+            //AssertRegistration(OrionRegistration registration, int orionid, int team, int target, int shooterid)
+
             var result = OrionGenerate.GenerateOrionForShooter(leonPerson, settings.OrionSetting);
 
-            Assert.IsTrue(result.Count == 6);
-            this.AssertRegistration(result[0], 1, 1, 1, 123);
-            this.AssertRegistration(result[1], 1, 3, 5, 123);
-            this.AssertRegistration(result[2], 1, 5, 9, 123);
-            this.AssertRegistration(result[3], 2, 1, 1, 123);
-            this.AssertRegistration(result[4], 3, 1, 1, 123);
-            this.AssertRegistration(result[5], 4, 1, 1, 123);
+            Assert.IsTrue(result.Count == 5);
+            this.AssertRegistration(result[0], 2, 1, 1, 123);
+            this.AssertRegistration(result[1], 2, 3, 5, 123);
+            this.AssertRegistration(result[2], 3, 5, 1, 123);
+            this.AssertRegistration(result[3], 3, 7, 4, 123);
+            this.AssertRegistration(result[4], 2, 9, 14, 123);
+            
         }
 
         [TestMethod]
         public void SecondTarget_FirstTeam()
         {
-            var settings = TestDataHelper.GetSettings("FeltAdminTest.TestData.FeltAdmin.xml");
+            var settings = TestDataHelper.GetSettings("FeltAdminTest.TestData.FeltAdmin2OrionPauses.xml");
+
             var leonPerson = new LeonPerson
             {
                 Class = "4",
@@ -72,7 +83,7 @@ namespace FeltAdminTest.Orion
         [TestMethod]
         public void LastTarget_FirstTeam()
         {
-            var settings = TestDataHelper.GetSettings("FeltAdminTest.TestData.FeltAdmin.xml");
+            var settings = TestDataHelper.GetSettings("FeltAdminTest.TestData.FeltAdmin2OrionPauses.xml");
 
             var leonPerson = new LeonPerson
             {
@@ -100,7 +111,7 @@ namespace FeltAdminTest.Orion
         [TestMethod]
         public void LastTarget_SecondTeam()
         {
-            var settings = TestDataHelper.GetSettings("FeltAdminTest.TestData.FeltAdmin.xml");
+            var settings = TestDataHelper.GetSettings("FeltAdminTest.TestData.FeltAdmin2OrionPauses.xml");
 
             var leonPerson = new LeonPerson
             {
@@ -125,21 +136,6 @@ namespace FeltAdminTest.Orion
             this.AssertRegistration(result[5], 4, 2, 4, 123);
         }
 
-        [TestMethod]
-        public void TestEncoding()
-        {
-            var filename = @"D:\Test\Grovfelt\FeltAdmin\KMINEW.TXT";
-            var ofilename = @"D:\KME\Orion2\Data\KMINEW.TXT.tmp";
-            var bytes = File.ReadAllBytes(filename);
-            var obytes = File.ReadAllBytes(ofilename);
-
-            Encoding iso = Encoding.GetEncoding("ISO-8859-1");
-            Encoding utf8 = Encoding.UTF8;
-            byte[] utfBytes = utf8.GetBytes("Øystein");
-            byte[] isoBytes = Encoding.Convert(utf8, iso, utfBytes);
-            string msg = iso.GetString(isoBytes);
-            
-        }
 
         private void AssertRegistration(OrionRegistration registration, int orionid, int team, int target, int shooterid)
         {
