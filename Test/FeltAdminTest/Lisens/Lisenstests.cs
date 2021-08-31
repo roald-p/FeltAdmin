0144910b15129b73828c39dedc1c526a38695856
Roald Parelius <rpa@dips.no>
2021-08-31 21:37:01 +0200

    lisens Bø skytterlag

--- Test/FeltAdminTest/Lisens/Lisenstests.cs
+++ Test/FeltAdminTest/Lisens/Lisenstests.cs
@@ -9,10 +9,32 @@ namespace FeltAdminTest.Lisens
     public class Lisenstests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Overhalla()
         {
-            var lisens = LisensChecker.GenerateLisens("Overhalla Skytterlag", new DateTime(2021, 08, 23));
+            var lisens = LisensChecker.GenerateLisens("Overhalla Skytterlag", new DateTime(2022, 04, 25));
             Assert.IsTrue(LisensChecker.Validate("Overhalla Skytterlag", DateTime.Now, lisens));
+        }
+
+        [TestMethod]
+        public void Hammerfest()
+        {
+            var lisens = LisensChecker.GenerateLisens("Hammerfest Skytterlag", new DateTime(2022, 12, 31));
+            Assert.IsTrue(LisensChecker.Validate("Hammerfest Skytterlag", DateTime.Now, lisens));
+        }
+
+        [TestMethod]
+        public void Bø()
+        {
+            var lisens = LisensChecker.GenerateLisens("Bø Skytterlag", new DateTime(2022, 12, 31));
+            Assert.IsTrue(LisensChecker.Validate("Bø Skytterlag", DateTime.Now, lisens));
+        }
+
+        [TestMethod]
+        public void CheckLisens()
+        {
+            //var lisens = LisensChecker.GenerateLisens("Overhalla Skytterlag", new DateTime(2021, 08, 23));
+            var result = LisensChecker.Validate("Bodø-Østre Skytterlag", DateTime.Now, "Qk9Ew5jDmFNUUkUgU0tZVFRFUkxBRzsyMDMwLTA2LTMwVDAwOjAwOjAw");
+            Assert.IsTrue(result);
         }
     }
 }
